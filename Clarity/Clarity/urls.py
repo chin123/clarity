f862cbc4114b5da746948d1ea17811f666596703
Chinmaya Krishnan Mahesh <chinmaya.mahesh@gmail.com>
2016-12-01 11:58:24 +0530

    Linked search and result page

--- Clarity/Clarity/urls.py
+++ Clarity/Clarity/urls.py
@@ -19,4 +19,5 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'', include('Search.urls')),
+    url(r'^results/', include('Results.urls')),
 ]
--- Clarity/Results/views.py
+++ Clarity/Results/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+def results(request):
+	if request.method == "POST":
+		a = request.POST.get("search")		
+		print(a)
+		return render(request, 'Results/index.html', {})
+	else:
+		return render(request, 'Results/index.html', {})
